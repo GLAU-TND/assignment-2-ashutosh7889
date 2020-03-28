bb4b7f61aea59f90355cbe324f68abfdf3eb0427
ashu <ashutosh.kuma_cs18>
2020-03-28 15:50:12 +0530

    add variable numberOfRecords of type int.....

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -15,5 +15,7 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         MyCircularQueue circularQueue = new MyCircularQueue();
+        System.out.println("Enter number of records you wants to enter");
+        int numberOfRecords = scan.nextInt();
     }
 }
