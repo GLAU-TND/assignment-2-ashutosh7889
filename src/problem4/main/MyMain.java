4b87372686ff7ca14539b683e9f73d951be12eda
ashu <ashutosh.kuma_cs18>
2020-03-28 13:59:44 +0530

    add an arraylist.....

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,8 +7,10 @@
 package problem4.main;
 
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 import problem4.myqueue.MyQueue;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // executable class
@@ -20,6 +22,13 @@ public class MyMain {
         MyQueue queue = new MyQueue();
         System.out.println("Enter number of nodes ");
         int numberOfNodes = scan.nextInt();
-
+        while (numberOfNodes-- > 0) {
+            System.out.println("Enter data");/*--->*/
+            int data = scan.nextInt();
+            TreeNode treeNode = new TreeNode();
+            treeNode.setData(data);
+            bst.insert(treeNode);
+        }
+        ArrayList<Integer> arrayList = bst.getPreOrderList();
     }
 }
