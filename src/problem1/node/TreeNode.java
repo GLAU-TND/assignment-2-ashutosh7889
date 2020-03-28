b4c6793f0a52fb0120c99a8b3d79ce4e3e99ea44
ashu <ashutosh.kuma_cs18>
2020-03-28 09:45:00 +0530

    add setter and getter method for leftnode and rightnode variable...

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -18,4 +18,20 @@ public class TreeNode {
     public void setData(int data) {
         this.data = data;
     }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
+    }
 }
