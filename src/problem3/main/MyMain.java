ef7e696ebbd9666543a4367af754c75917531951
ashu <ashutosh.kuma_cs18>
2020-03-28 13:02:53 +0530

    problem 3 completed...final commit...

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -7,6 +7,8 @@
 package problem3.main;
 
 import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
 
 import java.util.Scanner;
 
@@ -18,5 +20,26 @@ public class MyMain {
         MyPriorityQueue priorityQueue = new MyPriorityQueue();
         System.out.println("Enter number of nodes ");
         int nodes = scan.nextInt();
+        while (nodes-- > 0) {
+            Student student = new Student();
+            System.out.println("Enter name ");/*--->*/
+            String name = scan.next().trim();
+            System.out.println("Enter roll number");/*--->*/
+            int roll = scan.nextInt();
+            student.setName(name);
+            student.setRollNumber(roll);
+
+            Node node = new Node();
+            node.setStudent(student);
+
+            priorityQueue.enQueue(node);
+        }
+        System.out.println("Displaying Queue");
+
+        Node node = priorityQueue.deQueue();
+        while (node != null) {
+            System.out.println(node);
+            node = priorityQueue.deQueue();
+        }
     }
 }
